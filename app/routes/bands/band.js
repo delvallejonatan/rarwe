b00590165af084cb9b3070cf9cb8a51b60202bd1
delvallejonatan <delvallejonatan@gmail.com>
2017-02-07 00:38:42 -0300

    Add create band and song input

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    var bands = this.modelFor('bands');
+    let bands = this.modelFor('bands');
     return bands.findBy('slug', params.slug);
   }
 });
--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
+import Song from '../../../models/song';
 
 export default Ember.Route.extend({
-  model: function() {
+  model() {
     return this.modelFor('bands.band');
+  },
+  actions: {
+    createSong() {
+      let controller = this.get('controller');
+      let band = this.modelFor('bands.band');
+      let title = controller.get('title');
+
+      var song = Song.create({ title: title, band: band });
+      band.get('songs').pushObject(song);
+      controller.set('title', '');
+    }
   }
 });
