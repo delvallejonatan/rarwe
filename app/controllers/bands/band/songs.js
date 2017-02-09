2f00cd26a77a7068b657596ec1e98600f264f780
delvallejonatan <delvallejonatan@gmail.com>
2017-02-09 01:37:30 -0300

    Create bands controller

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  title: '',
+  isAddButtonDisabled: Ember.computed('title', function() {
+    return Ember.isEmpty(this.get('title'));
+  }),
   actions: {
     updateRating(params) {
       let song = params.item,
