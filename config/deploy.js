e4c47dc5f1698e0f7134ddd32a469d5c2dbeae95
delvallejonatan <delvallejonatan@gmail.com>
2017-07-26 23:37:21 -0300

    Fix deploy

--- config/deploy.js
+++ config/deploy.js
@@ -1,10 +1,6 @@
 module.exports = function(deployTarget) {
   var ENV = {
     build: {},
-    pagefront: {
-      app: 'rarwe',
-      key: process.env.PAGEFRONT_KEY
-    }
   };
   ENV.build.environment = 'production';
 
