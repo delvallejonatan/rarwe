f3bdb2b8adb2a2165adf0e2be9cb7dead81b82f3
delvallejonatan <delvallejonatan@gmail.com>
2017-11-29 14:58:04 +0100

    chane api host

--- config/environment.js
+++ config/environment.js
@@ -21,7 +21,7 @@ module.exports = function(environment) {
       // Here you can pass flags/options to your application instance
       // when it is created
     },
-    apiHost: 'https://json-api.rockandrollwithemberjs.com',
+    apiHost: 'https://cors-anywhere.herokuapp.com/http://json-api.rockandrollwithemberjs.com'
   };
 
   if (environment === 'development') {
