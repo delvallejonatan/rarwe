b760545d6ad2a42051648f47af60cd4c8fb63ad5
delvallejonatan <delvallejonatan@gmail.com>
2017-11-29 15:16:52 +0100

    allow origin headers

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,11 @@
-import DS from 'ember-data';
-import config from 'rarwe/config/environment';
+import DS from 'ember-data'
+import config from 'rarwe/config/environment'
 
 export default DS.JSONAPIAdapter.extend({
   host: config.apiHost,
-});
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  },
+})
 
 // API lived at http://rock-and-roll-with-emberjs-api.herokuapp.com
